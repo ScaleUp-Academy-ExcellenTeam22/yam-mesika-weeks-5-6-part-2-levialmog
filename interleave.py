e495322a334824baa1540c7b720fe109e2b0bcca
Almog <almoglevi1008@gmail.com>
2022-04-12 17:39:37 +0300

    Code fixed according to the comment

--- interleave.py
+++ interleave.py
@@ -1,11 +1,12 @@
 def interleave(*iterables):
     """
-    The function in each call sends the objects in the same location from each iterable.
-    :param iterables: The iterable objects that interleave for one list.
+    The function gets iterables, in each call sends the items in the same index.
+    :param iterables: The iterable objects to be interleaved for one list.
+    :return: A list of items in the same index from iterables.
     """
-    for index in range(0, len(iterables)):
-       yield [iterable[index] for iterable in iterables]
-       index += 1
+    for item in range(0, len(iterables)):
+        yield [iterable[item] for iterable in iterables]
+        item += 1
 
 
 if __name__ == "__main__":
